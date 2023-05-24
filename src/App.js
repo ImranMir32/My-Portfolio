0f8ff2ea5d655b28e6cc44f3277f9a3eefc10bf1
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-24 22:54:37 +0600

    adding routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Resume from "./containers/resume";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/containers/portfolio/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Portfolio = () => {
+  return <div>Protfolio Page</div>;
+};
+export default Portfolio;
