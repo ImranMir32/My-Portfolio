720bef14ae615a09890a04cd945e4dd37e4f4f01
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-25 01:49:11 +0600

    adding particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+// import "./App.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "./containers/home";
 import About from "./containers/about";
 import Resume from "./containers/resume";
@@ -7,19 +9,40 @@ import Skills from "./containers/skills";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
+import particles from "./helper/particles";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
+
+  const handleInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const renderParticleJsInHomePage = location.pathname === "/";
+
   return (
     <div className="App">
+      {/* particles js */}
+
+      {renderParticleJsInHomePage && (
+        <Particles id="particles" options={particles} init={handleInit} />
+      )}
+
+      {/* navbar */}
       <Navbar />
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+
+      {/* main page content */}
+      <div className="App__main-page-content">
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
