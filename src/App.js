c1c5d39673507a41af1ac4285a419de05a09c7d3
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-25 00:18:58 +0600

    adding root style and navbar components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Resume from "./containers/resume";
 import Skills from "./containers/skills";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
+import Navbar from "./components/navBar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
