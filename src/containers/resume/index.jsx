59ab5a396eb8443dccabb55d8834f68f60c02f95
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-25 17:51:26 +0600

    adding resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,6 +1,89 @@
 import React from "react";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { FaUserTie } from "react-icons/fa";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { data } from "./utils";
+import "./styles.scss";
+import { MdWork } from "react-icons/md";
 
 const Resume = () => {
-  return <div>Resume Page</div>;
+  return (
+    <section id="resume" className="resume">
+      <PageHeaderContent
+        headerText="My Resume"
+        icon={<FaUserTie size={40} />}
+      />
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__header-text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date={item.duration}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.company}{" "}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date={item.duration}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.institute}{" "}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Resume;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,31 @@
+export const data = {
+  experience: [
+    {
+      title: "Software Engineer Trainee",
+      company: "Brain Station-23",
+      duration: "07/2022-11/2022",
+    },
+    {
+      title: "Programming Instructor and Mentor",
+      company: "University of Information Technology & Sciences",
+      duration: "2021-Present",
+    },
+  ],
+  education: [
+    {
+      title: "B.Sc. In Computer Science & Engineering",
+      institute: "University of Information Technology & Sciences",
+      duration: "2019-Present",
+    },
+    {
+      title: "Higher Secondary School Certificate",
+      institute: "Kadamtola Purbo Bashabo School & College",
+      duration: "2016-2018",
+    },
+    {
+      title: "Secondary School Certificate",
+      institute: "Tajuddin Adarsha High School",
+      duration: "2005-2015",
+    },
+  ],
+};
