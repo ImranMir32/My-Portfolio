11033c996e523fbb8192b1b2e257e9a9c7cf7d6f
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-25 21:22:54 +0600

    working on home page adding download resume and social icone

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Animate } from "react-simple-animate";
 import "./styles.scss";
+import Resume from "../../assets/Md. Imran Mir.pdf";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { SiCodeforces, SiLeetcode } from "react-icons/si";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -32,6 +35,59 @@ const Home = () => {
       >
         <div className="home__contact-me">
           <button onClick={handleNavigateToContactMePage}>Hire Me</button>
+
+          <a href={Resume} download="Resume">
+            <button>Download Resume</button>
+          </a>
+        </div>
+
+        <div className="home__social">
+          <div className="home__social__icon">
+            <a
+              href="https://www.linkedin.com/in/md-imran-mir-972997206/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {<FaLinkedin size={40} color="var( --yellow-theme-main-color)" />}
+            </a>
+          </div>
+          <div className="home__social__icon">
+            <a
+              href="https://github.com/ImranMir32"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {
+                <FaGithubSquare
+                  size={40}
+                  color="var( --yellow-theme-main-color)"
+                />
+              }
+            </a>
+          </div>
+          <div className="home__social__icon">
+            <a
+              href="https://codeforces.com/profile/Imran32"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {
+                <SiCodeforces
+                  size={40}
+                  color="var( --yellow-theme-main-color)"
+                />
+              }
+            </a>
+          </div>
+          <a
+            href="https://leetcode.com/Imran32/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="home__social__icon">
+              {<SiLeetcode size={40} color="var( --yellow-theme-main-color)" />}
+            </div>
+          </a>
         </div>
       </Animate>
     </section>
