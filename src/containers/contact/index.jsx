9044a0ebfd37a809cddb0c161dc5e9056a3d76b7
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-27 01:39:56 +0600

    adding emailjs and toastify in contact section

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { MdOutlineContactPhone } from "react-icons/md";
 import { Animate } from "react-simple-animate";
 import "./styles.scss";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_n4ch7u1",
+        "template_6410oas",
+        form.current,
+        "p2m_dLhVVqi6U-zfq"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  const showToastMessage = () => {
+    toast.success("Successfully send !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
   return (
     <section id="contact" className="contact">
       <PageHeaderContent
@@ -42,43 +75,48 @@ const Contact = () => {
           }}
         >
           <div className="contact__content__form">
-            <div className="contact__content__form__controlswrapper">
-              <div>
-                <input
-                  required
-                  name="name"
-                  className="inputName"
-                  type={"text"}
-                />
-                <label htmlFor="name" className="nameLabel">
-                  Name
-                </label>
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="contact__content__form__controlswrapper">
+                <div>
+                  <input
+                    required
+                    name="user_name"
+                    className="inputName"
+                    type={"text"}
+                  />
+                  <label htmlFor="name" className="nameLabel">
+                    Name
+                  </label>
+                </div>
+                <div>
+                  <input
+                    required
+                    name="user_email"
+                    className="inputEmail"
+                    type={"text"}
+                  />
+                  <label htmlFor="email" className="emailLabel">
+                    Email
+                  </label>
+                </div>
+                <div>
+                  <textarea
+                    required
+                    name="message"
+                    className="inputDescription"
+                    type={"text"}
+                    rows="5"
+                  />
+                  <label htmlFor="description" className="descriptionLabel">
+                    Description
+                  </label>
+                </div>
               </div>
-              <div>
-                <input
-                  required
-                  name="email"
-                  className="inputEmail"
-                  type={"text"}
-                />
-                <label htmlFor="email" className="emailLabel">
-                  Email
-                </label>
-              </div>
-              <div>
-                <textarea
-                  required
-                  name="description"
-                  className="inputDescription"
-                  type={"text"}
-                  rows="5"
-                />
-                <label htmlFor="description" className="descriptionLabel">
-                  Description
-                </label>
-              </div>
-            </div>
-            <button>Submit</button>
+              <button type="submit" value="Send" onClick={showToastMessage}>
+                Submit
+              </button>
+              <ToastContainer />
+            </form>
           </div>
         </Animate>
       </div>
